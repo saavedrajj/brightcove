28ae5b2a06d3bebb0fe41e242d71cca00c4507b7
Juan Saavedra <saavedrajj@gmail.com>
2019-01-23 10:07:48 +0000

    d

--- test/clear-log.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $logFileLocation = "di.json";
-    $freshContent = array ();
-    $encodedContent = json_encode($freshContent);
-    file_put_contents($logFileLocation, $encodedContent);
-
-echo 'Log file cleared - <a href="di-log.html">go back to the dashboard</a>';
-?>
